ac5219b57664f308509600199e6af713d12d8b5b
SONG JUNBONG <53103084+madgenerator@users.noreply.github.com>
2024-08-10 14:22:29 +0000

    CORS update

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
+// CORS 허용
+app.use(cors());
+
+// 모든 요청에 대한 로그 출력
 app.use((req, res, next) => {
   console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url}`);
   next();
@@ -9,15 +14,11 @@ app.use((req, res, next) => {
 
 // GET 요청에 대한 핸들러
 app.get('/', (req, res) => {
-  // 요청 파라미터에서 "name"과 "age" 추출
-  const name = req.query.name || 'Unknown';
-  const age = req.query.age || 'Unknown';
-
-  // 응답
+  const { name = 'Unknown', age = 'Unknown' } = req.query;
   res.send(`당신의 이름은 ${name}이고, ${age} 살입니다.`);
 });
 
 // 서버 시작
 app.listen(port, () => {
-  console.log(`서버가 http://localhost:${port} 포트에서 실행 ggg중입니다.`);
+  console.log(`서버가 http://localhost:${port} 포트에서 실행 중입니다.`);
 });
